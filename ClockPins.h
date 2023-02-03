926089d0a1b340d6b3a8b3c91c9dc9006f2cc0fb
Francesco Comi <francesco.comi@gmail.com>
2023-02-03 16:11:31 +0100

    Fixed reversed direction for the new distribution board Initial support for master/slave boards Added SETCOUNT and SETHOME commands Other minor fixes

--- ClockPins.h
+++ ClockPins.h
@@ -19,34 +19,34 @@
 
 static unsigned short pins[48][3] = { // {DIR, STEP, REVERSED}
     // Board 0
-    {  6, 13, 1},         // HOURS   TENS  TOPLEFT     HOURS_HAND
-    {  7, 12, 0},         //                           MINUTES_HAND
+    {  6, 13, 0},         // HOURS   TENS  TOPLEFT     HOURS_HAND
+    {  7, 12, 1},         //                           MINUTES_HAND
     {  8, 11, 1},         //               TOPRIGHT    HOURS_HAND
     {  9, 10, 0},         //                           MINUTES_HAND
     // Board 1
-    { 21,  5, 1},         //               MIDDLELEFT  HOURS_HAND
-    { 20,  4, 0},         //                           MINUTES_HAND
+    { 21,  5, 0},         //               MIDDLELEFT  HOURS_HAND
+    { 20,  4, 1},         //                           MINUTES_HAND
     { 19,  3, 1},         //               MIDDLERIGHT HOURS_HAND
     { 18,  2, 0},         //                           MINUTES_HAND
     // Board 2
-    { 29, 22, 1},         //               BOTTOMLEFT  HOURS_HAND
-    { 28, 23, 0},         //                           MINUTES_HAND
+    { 29, 22, 0},         //               BOTTOMLEFT  HOURS_HAND
+    { 28, 23, 1},         //                           MINUTES_HAND
     { 27, 24, 1},         //               BOTTOMRIGHT HOURS_HAND
     { 26, 25, 0},         //                           MINUTES_HAND
 
     // Board 3
-    { 30, 37, 1},         // MINUTES  TENS TOPLEFT     HOURS_HAND
-    { 31, 36, 0},         //                           MINUTES_HAND
+    { 30, 37, 0},         // MINUTES  TENS TOPLEFT     HOURS_HAND
+    { 31, 36, 1},         //                           MINUTES_HAND
     { 32, 35, 1},         //               TOPRIGHT    HOURS_HAND
     { 33, 34, 0},         //                           MINUTES_HAND
     // Board 4
-    { 38, 45, 1},         //               MIDDLELEFT  HOURS_HAND
-    { 39, 44, 0},         //                           MINUTES_HAND
+    { 38, 45, 0},         //               MIDDLELEFT  HOURS_HAND
+    { 39, 44, 1},         //                           MINUTES_HAND
     { 40, 43, 1},         //               MIDDLERIGHT HOURS_HAND
     { 41, 42, 0},         //                           MINUTES_HAND
     // Board 5
-    { 46, 50, 1},         //               BOTTOMLEFT  HOURS_HAND
-    { 47, 51, 0},         //                           MINUTES_HAND
+    { 46, 50, 0},         //               BOTTOMLEFT  HOURS_HAND
+    { 47, 51, 1},         //                           MINUTES_HAND
     { 48, 52, 1},         //               BOTTOMRIGHT HOURS_HAND
     { 49, 53, 0},         //                           MINUTES_HAND
 

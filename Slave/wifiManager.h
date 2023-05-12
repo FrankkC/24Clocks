788632fa93672f6380117602a58ba25785938eee
Francesco Comi <francesco.comi@gmail.com>
2023-05-12 13:47:33 +0200

    Maybe it's better to have separated Master and Slave sketches

---
+++ Slave/wifiManager.h
@@ -0,0 +1,25 @@
+#ifndef wifiManager_h
+#define wifiManager_h
+#include <Arduino.h>
+
+#define TIMEOUT 10000 // mS
+
+class WifiManager {
+
+    public:
+
+        WifiManager();
+        static void init();
+        static void sendData(String str);
+        static String readCommand();
+
+    private:
+
+        static bool sendCommand(String cmd, String ack = "", String error = "");
+        static bool echoFind(String keyword, String error);
+        static bool initWifi();
+        static bool initServer();
+
+};
+
+#endif
